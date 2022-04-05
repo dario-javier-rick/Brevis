3530db7b4e0e17cdbd951de03a2c08562c3d157b
Dario Javier Rick <Dario.Rick@emerios.com>
2022-04-05 18:56:12 -0300

    Web project

---
+++ EstudiarEsElCamino.Web/Pages/Index.cshtml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using EstudiarEsElCamino.Core.BusinessProcess;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace EstudiarEsElCamino.Web.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private SubjectBusinessProcess _subjectBusinessProcess { get; set; }
+
+        public void OnGet()
+        {
+
+        }
+
+
+        [BindProperty]
+        public IFormFile File { get; set; }
+        public async Task<IActionResult> OnPostAsync()
+        {
+            //Getting file meta data
+            var fileName = Path.GetFileName(File.FileName);
+            var contentType = File.ContentType;
+
+            // to do something with the above data
+            //_subjectBusinessProcess....
+
+
+            // to do : return something
+            throw new NotImplementedException();
+        }
+    }
+}
