6b57ee65ede10b104c19607c18f4c2071d4c64b3
J. Nicolás Videla Rivero <Nyko-94@hotmail.com>
2022-04-17 15:11:10 -0300

    Add a resource to store the path of the default study plan Update the net standar version to 2.0 to allow use Newtonsoft library Add ReadStudyPlanFromJson method in the StudyPlan Class Add Newtonsoft.Json and System.IO.FileSystem into the Core project

--- EstudiarEsElCamino.Core/Model/StudyPlan.cs
+++ EstudiarEsElCamino.Core/Model/StudyPlan.cs
@@ -1,7 +1,9 @@
 ﻿namespace EstudiarEsElCamino.Core.Model
 {
+    using Newtonsoft.Json.Linq;
     using System.Collections.Generic;
-    
+    using System.IO;
+
     public class StudyPlan
     {
 
@@ -10,6 +12,13 @@
         public StudyPlan(IEnumerable<Correlativities> correlativities)
         {
             this.Correlativities = correlativities;
+        }
+
+        public StudyPlan ReadStudyPlanFromJson(string path) 
+        {
+            var jsonStringFromFile = File.ReadAllText(path);
+            var studyPlanFromJson = Newtonsoft.Json.JsonConvert.DeserializeObject<StudyPlan>(jsonStringFromFile);
+            return studyPlanFromJson;
         }
     }
 }
