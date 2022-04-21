e18d07c32dcfacd92d015e550f771b0f8304a0b8
alenelis <alenelis@gmail.com>
2022-04-21 17:02:00 -0300

    Se agrego y modifico el diagrama de las clases

---
+++ EstudiarEsElCamino.Core/LoadJson.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+
+namespace EstudiarEsElCamino.Core
+{
+    public static class JsonFileReader
+    {
+        public static T Read<T>(string filePath)
+        {
+            string text = File.ReadAllText(filePath);
+            return JsonSerializer.Deserialize<T>(text);
+        }
+    }
