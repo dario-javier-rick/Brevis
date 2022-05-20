e6fe9f495bb0dc2d66f602242dbf52ee30d73234
alenelis <alenelis@gmail.com>
2022-05-20 00:44:07 -0300

    Se agrego una funcion que enera el Json

--- Brevis.Web/Pages/Tree.cshtml.cs
+++ Brevis.Web/Pages/Tree.cshtml.cs
@@ -1,23 +1,37 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 using Brevis.Core.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Newtonsoft.Json;
 
 namespace Brevis.Web.Pages
 {
     public class TreeModel : PageModel
     {
-        public IEnumerable<Subject> _subjects ;
+        public string _jsonSubjects ;
 
 
         public void OnGet(IEnumerable<Subject> subjects)
-        {
+         {
             //_subjects = subjects;
-            _subjects = new List<Subject> { new Subject { Code = "A" } };
+            var _subjects = new List<Subject> { 
+                 new Subject { Code = "1", Name="A" }
+                , new Subject { Code = "2", Name="B" }
+                , new Subject { Code = "3", Name="C" }
+                };
 
+            var returnedSubjects = _subjects.Select(t =>
+                            new {
+                                Id = Int16.Parse(t.Code),
+                                Label = t.Name
+                            }).ToArray();
+            Encoding.GetEncoding(
+
+            _jsonSubjects = JsonConvert.SerializeObject(returnedSubjects);
         }
     }
 }
