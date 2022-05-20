0b111c54032db8d4c70eb25e9d72608550706871
Dario Javier Rick <Dario.Rick@emerios.com>
2022-05-20 16:22:46 -0300

    US03 Lector de Archivos XLSX. Brevis.Importer.XlsReader

---
+++ Brevis.Importer.XlsReader/XlsProgressCarreerTransformer.cs
@@ -0,0 +1,17 @@
+﻿using Brevis.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Brevis.Importer.XlsReader
+{
+    public class XlsProgressCarreerTransformer : Core.IProgressCarreerTransformer
+    {
+        public ProgressCarreer Transform(object input)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
