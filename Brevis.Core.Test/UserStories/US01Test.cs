10afe86648f6dd318deb14278d17fec28d5b7bd9
Alejandro Nelis <anelis@tecnosoftware.com>
2022-06-21 13:56:59 -0300

    Se agregaron más test

--- Brevis.Core.Test/UserStories/US01Test.cs
+++ Brevis.Core.Test/UserStories/US01Test.cs
@@ -24,12 +24,67 @@ namespace Brevis.Core.Test.UserStories
         public void CA1()
         {
             //Arrange
-            var criticalPath = this.model.GetCriticalStudyPath(new ProgressCarreer());
-            
+            Subject subject = new Subject();
+            ProgressCarreer progressCarreer = new ProgressCarreer();
+            progressCarreer.CurriculumCode = "1";
+            //          pg.ApprovedSubjects.Add(subject);
+            var criticalPath = this.model.GetCriticalStudyPath(progressCarreer);
             //Act
 
-            //Assert 
-            Assert.IsTrue(criticalPath.Count == 0);
+            var criticalStudyPath = this.model.GetCriticalStudyPath(new ProgressCarreer());
+            criticalStudyPath.Add(SubjectMocks.B);
+            criticalStudyPath.Add(SubjectMocks.C);
+            //Assert
+            Assert.AreEqual(criticalPath, criticalStudyPath);
         }
+        [Test]
+        public void CA2()
+        {
+            //Arrange
+            ProgressCarreer progressCarreer = new ProgressCarreer();
+            progressCarreer.ApprovedSubjects.Add(SubjectMocks.A);
+            progressCarreer.ApprovedSubjects.Add(SubjectMocks.B);
+            progressCarreer.ApprovedSubjects.Add(SubjectMocks.C);
+            progressCarreer.CurriculumCode = "1";
+
+            //Act
+            
+            var criticalPath = this.model.GetCriticalStudyPath(new ProgressCarreer());
+
+            //Assert 
+            Assert.IsTrue( criticalPath.Count == 0);
+        }
+        [Test]
+        public void CA3()
+        {
+            //Arrange
+            ProgressCarreer progressCarreer = new ProgressCarreer();
+            progressCarreer.ApprovedSubjects.Add(SubjectMocks.B);
+            //Act
+            var criticalPath = this.model.GetCriticalStudyPath(new ProgressCarreer());
+
+            //Assert 
+            var criticalStudyPath = this.model.GetCriticalStudyPath(new ProgressCarreer());
+            criticalStudyPath.Add(SubjectMocks.A);
+            criticalStudyPath.Add(SubjectMocks.B);
+            Assert.AreEqual(criticalPath, criticalStudyPath);
+        }
+        [Test]
+        public void CA4()
+        {
+            //Arrange
+            ProgressCarreer progressCarreer = new ProgressCarreer();
+            progressCarreer.ApprovedSubjects.Add(SubjectMocks.B);
+            //Act
+            var criticalPath = this.model.GetCriticalStudyPath(new ProgressCarreer());
+
+            //Assert
+            
+//            var criticalStudyPath = this.model.GetCriticalStudyPath(new ProgressCarreer());
+//            criticalStudyPath.Add(SubjectMocks.A);
+
+            Assert.AreEqual(criticalPath, SubjectMocks.A);
+        }
+
     }
 }
