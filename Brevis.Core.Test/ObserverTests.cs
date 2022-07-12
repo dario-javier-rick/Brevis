368e2dd64fb64fff9f7cf0ed034ff9865965b0af
Dario Javier Rick <Dario.Rick@emerios.com>
2022-07-12 17:13:20 -0300

    Improve tests

---
+++ Brevis.Core.Test/ObserverTests.cs
@@ -0,0 +1,28 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Brevis.Core.Test
+{
+    [TestFixture]
+    public class ObserverTests
+    {
+        Observer observer;
+
+        [SetUp]
+        public void Setup()
+        {
+            //observer = new Observer();
+        }
+
+        [Test]
+        public void NullArgumentTest()
+        {
+            Assert.Throws<NullReferenceException>(() =>
+            {
+                observer.Update();
+            });
+        }
+    }
+}
---
+++ Brevis.CoreTests/ObserverTests.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Brevis.CoreTests
+{
+    internal class ObserverTests
+    {
+    }
+}
