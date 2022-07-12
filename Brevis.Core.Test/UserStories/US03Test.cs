a8065117bd35b997993ef7408fe5971ad9a9a08a
Dario Javier Rick <Dario.Rick@emerios.com>
2022-07-11 22:22:21 -0300

    US03 tests

--- Brevis.Core.Test/UserStories/US03Test.cs
+++ Brevis.Core.Test/UserStories/US03Test.cs
@@ -1,17 +1,25 @@
-﻿using NUnit.Framework;
+﻿using Brevis.Core.Models;
+using NUnit.Framework;
+using System;
+using System.IO;
 
 namespace Brevis.Core.Test.UserStories
 {
     [TestFixture]
     public class US03Test
     {
+        private Discover discover;
+
         [SetUp]
         public void Setup()
         {
+            discover = new Discover();
         }
+
         [TearDown]
         public void TearDown()
         {
+            discover = null;
         }
 
         /// <summary>
@@ -20,7 +28,14 @@ namespace Brevis.Core.Test.UserStories
         [Test]
         public void CA01Test()
         {
-            //TODO...
+            //Arrange
+            var path = Directory.GetCurrentDirectory() + "/UserStories/Files/US03/CA01"; ;
+
+            //Act
+            var result = discover.GetProgressCarreerTransformers(path);
+
+            //Assert
+            Assert.IsTrue(result.ContainsKey("Brevis.Importer.XlsReader.XlsProgressCarreerTransformer"));
         }
 
         /// <summary>
@@ -29,7 +44,36 @@ namespace Brevis.Core.Test.UserStories
         [Test]
         public void CA02Test()
         {
-            //TODO...
+            //Arrange
+            var path = Directory.GetCurrentDirectory() + "/UserStories/Files/US03/CA02";
+
+            var assemblyToUse = "Brevis.Importer.XlsReader.XlsProgressCarreerTransformer";
+            var fileToProcess = path + "/approved_subjects.xlsx";
+
+            //Act
+            var progressCarreerTransformerImplementations = discover.GetProgressCarreerTransformers(path);
+            var intent = progressCarreerTransformerImplementations.TryGetValue(assemblyToUse, out var progressCarreerTransformer);
+
+            if (!intent)
+            {
+                throw new ArgumentException($"There was an error trying to resolve an IProgressCarreerTransformer. Key: {assemblyToUse}");
+            }
+
+            ProgressCarreer progressCarrer;
+
+            using (var fs = File.OpenRead(fileToProcess))
+            {
+                using (var ms = new MemoryStream())
+                {
+                    fs.CopyTo(ms);
+                    ms.Seek(0, SeekOrigin.Begin);
+
+                    progressCarrer = progressCarreerTransformer.Transform(ms);
+                }
+            }
+
+            //Assert
+            Assert.IsTrue(progressCarrer.CurriculumCode == "Plan");
         }
 
     }
